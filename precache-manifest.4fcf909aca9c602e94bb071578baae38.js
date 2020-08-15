c0d20b8928f1d48b88a2c84433f1e1885913744c
Paolo Di Dio <paolo.didio@studenti.unipd.it>
2020-08-16 01:12:05 +0200

    Updates

---
+++ precache-manifest.4fcf909aca9c602e94bb071578baae38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce19875c10a63c5181d75303539e6d41",
+    "url": "/chiara/index.html"
+  },
+  {
+    "revision": "8f43cd2b815a172ef9b3",
+    "url": "/chiara/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e68f839645911b92655",
+    "url": "/chiara/static/js/2.3b7cc3d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chiara/static/js/2.3b7cc3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f43cd2b815a172ef9b3",
+    "url": "/chiara/static/js/main.035c9a91.chunk.js"
+  },
+  {
+    "revision": "9f335da15dfab59a61d0",
+    "url": "/chiara/static/js/runtime-main.687108d4.js"
+  }
+]);
